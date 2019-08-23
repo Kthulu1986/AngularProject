3417a582967c75d197261feb88ddc124f6a86b99
Kthulu <inblooder@gmail.com>
2019-08-23 00:26:05 -0400

    Version3

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { DataDbService } from '../../services/data-db.service';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 @Component({
   selector: 'contactForm',
   templateUrl: './contact.component.html',
@@ -8,12 +8,14 @@ import { FormControl, FormGroup } from '@angular/forms';
 })
 export class ContactComponent implements OnInit {
 
+  emailPattern: any = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
   createFormsGroup () {
     return new FormGroup({
-      email: new FormControl(''),
-      name: new FormControl(''),
-      message: new FormControl('')
-    })
+      email: new FormControl('',[Validators.required,Validators.minLength(5),Validators.pattern(this.emailPattern)]),
+      name: new FormControl('',[Validators.required,Validators.minLength(5)]),
+      message: new FormControl('',[Validators.required,Validators.minLength(10),Validators.maxLength(100)])
+    });
 
   }
   contactForm: FormGroup;
@@ -27,14 +29,17 @@ export class ContactComponent implements OnInit {
     this.contactForm.reset();
   }
   onSaveForm(){
-    console.log('Saved', this.contactForm.value);
-    const newContact = {
-      name: 'Bazael',
-      email: 'me@gmail.com',
-      message: 'Hola Mundo'
-    }
+    if (this.contactForm.valid){
     this.dbData.saveMessage(this.contactForm.value);
-
+    this.onResetForm();
+    console.log("Validado");
+    }else{
+      console.log("No validado");
+    }
   }
+  
+  get name(){ return this.contactForm.get('name');}
+  get email(){ return this.contactForm.get('email');}
+  get message(){ return this.contactForm.get('message');}
 
 }
